d426e0180ca6f93e16d2ded78bcd7c4628d9ae4a
Vladimir <hotsru12@gmail.com>
2022-10-21 15:51:44 +0300

    cart-fixed

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,7 @@ import Home from './pages/Home';
 import './scss/app.scss';
 import MainLayout from './layouts/MainLayout';
 
-const Cart = Loadable({
-  loader: () => import(/* webpackChunkName: "Cart" */ './pages/Cart'),
-  loading: () => <div>Идёт загрузка корзины...</div>,
-});
+const Cart = React.lazy(() => import(/* webpackChunkName: "FullPizza" */ './pages/Cart'));
 
 const FullPizza = React.lazy(() => import(/* webpackChunkName: "FullPizza" */ './pages/FullPizza'));
 const NotFound = React.lazy(() => import(/* webpackChunkName: "NotFound" */ './pages/NotFound'));
